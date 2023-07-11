f8adff47e3e10080c412f377453462aaa15c7cdf
alirezahw <138487010+alirezahw@users.noreply.github.com>
2023-07-11 17:41:34 +0330

    Add files via upload

---
+++ tamrin7.py
@@ -0,0 +1,12 @@
+n=int(input("amroz che tarikhi ast?(sal): "))
+l=int(input("amroz che tarikhi ast?(mah): "))
+t=int(input("amroz che tarikhi ast?(roz): "))
+a=int(input("sal tavalod khod ra vared konid: "))
+b=int(input("mah tavalod khod ra vared konid: "))
+c=int(input("roz tavalod khod ra vared konid: "))
+s=n-a
+m=l-b
+r=t-c
+o=s*45975456
+print("sen shoma: ",s  ,m   ,r )
+print('sen shoma be sanie: ',o)
